ab7a527702c0d046d4e10194e7956691c7d95787
Luis Preza <preza.luis@gmail.com>
2020-03-03 14:30:37 -0600

    local style file changed, stickler last config changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: [
+    /* eslint-disable-next-line */
     require('postcss-import'),
+    /* eslint-disable-next-line */
     require('postcss-flexbugs-fixes'),
+    /* eslint-disable-next-line */
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009',
