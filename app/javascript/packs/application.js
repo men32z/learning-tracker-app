3231adec441ef8e90ca15abeccc17e230d03b570
Luis Preza <preza.luis@gmail.com>
2020-03-05 13:39:53 -0600

    some assets for app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
-import './style.scss';
+import '../assets/scss/style.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
