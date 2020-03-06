308b3051cf5ae898805b7eb8f37fe06098d7149c
Luis Preza <preza.luis@gmail.com>
2020-03-05 20:36:47 -0600

    home date and circles styles

--- app/javascript/components/temporalViews/Home.js
+++ app/javascript/components/temporalViews/Home.js
@@ -2,20 +2,34 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {library} from '../../assets/plugins/fas.js'
 import Circle from 'react-circle';
+import {colors} from '../../data/styles.js';
 
 export default function Home(){
+  const circles =  [10, 25, 80].map(x=>(
+    <div>
+      <Circle
+        responsive={true} // Boolean: Make SVG adapt to parent size
+        progress={x} // Number: Update to change the progress and percentage.
+        progressColor="#8EE289"  // String: Color of "progress" portion of circle.
+        textStyle={{
+          font: 'bold 7rem Helvetica, Arial, sans-serif'
+        }}
+        textColor={colors.grayDark} // String: Color of percentage text color.
+        showPercentageSymbol={false}
+      />
+      <h3>subject</h3>
+    </div>
+  ));
   return (
     <div className="bg-gray full-screen">
-      <div className="bg-white">
+      <div className="bg-white row">
         <div className="home-date">
           <div><FontAwesomeIcon icon={['fas', 'angle-left']} /></div>
-          <div>7 september 2013</div>
+          <div><b>7 september 2013</b></div>
           <div><FontAwesomeIcon icon={['fas', 'angle-right']} /></div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="home-circles">
+          {circles}
         </div>
       </div>
     </div>
